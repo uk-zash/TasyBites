ba29ffa37f2681b84411f417ada15f673f31a60b
uk-zash <ssik938@gmail.com>
2025-02-07 17:32:19 +0530

    updated app

--- src/app.js
+++ src/app.js
@@ -55,7 +55,7 @@ app.use('/', indexRoutes);
 app.use('/' , adminRoutes)
 
 // Start the server
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
